564e8087dab465b01f61c402c3429d23e3b5dccb
vahslive <avakhmianin@gmail.com>
2026-09-12 01:16:47 -0700

    Use COALESCE for master_name in receipt query

--- functions/api/receipt/[token].js
+++ functions/api/receipt/[token].js
@@ -4,7 +4,7 @@ export async function onRequestGet({ env, params }) {
   const booking = await env.DB.prepare(
     `SELECT b.id, b.name, b.address, b.dismount, b.size, b.bracket, b.wall, b.wires, b.addons, b.tvs_json,
             b.total_price, b.actual_total, b.payment_method, b.completed_at, b.booking_date, b.booking_time,
-            u.name AS master_name
+            COALESCE(u.display_name, u.name) AS master_name
      FROM bookings b
      LEFT JOIN users u ON u.id = b.claimed_by
      WHERE b.receipt_token = ?`
